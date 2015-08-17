1dce5b366aa81f6650493b6abbaea7df22b768b8
Michael Gillett <mgillett54@gmail.com>
2015-08-16 23:41:23 -0400

    Created language library with plural and titleize methods Count User at Location method throws an error if the eatery is invalid Added timer start to speech intent request Created a display result method to handle both text and speech result rendering

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,31 +3,33 @@ $(function() {
   $('.btn').click(function() {
     var query = $('.query').val();
     $.post('/query', {query: query}, function(data) {
-      $('.response').text(data.res);
-      $('.result').show().text(JSON.stringify(data.raw, null, 2));
-      var s_to_ms = data.time[0] * 1000;
-      var ns_to_ms = data.time[1] / 1000000;
-      $('.time').text(s_to_ms + ns_to_ms + 'ms');
+      displayResult(data);
     })
   })
 
   $(document).on('click', '.record-start', function() {
-    console.log('start')
     $(this).toggleClass('record-start record-stop')
+    $(this).children().toggleClass('fa-microphone fa-stop')
     $.post('/record-start', function(data) {
       console.log("recording started")
     })
   })
 
   $(document).on('click', '.record-stop', function() {
-    console.log('stop')
     $btn = $(this)
     $btn.toggleClass('record-start record-stop')
-    $btn.children().toggleClass('fa-microphone fa-spinner fa-spin')
+    $btn.children().toggleClass('fa-stop fa-spinner fa-spin')
     $.post('/record-stop', function(data) {
-      console.log("recording stopped")
       $btn.children().toggleClass('fa-microphone fa-spinner fa-spin')
-      $('.result').show().text(JSON.stringify(data.outcomes[0], null, 2))
+      displayResult(data)
     })
   })
+
+  function displayResult(data) {
+    $('.response').text(data.res);
+    $('.result').show().text(JSON.stringify(data.raw, null, 2));
+    var s_to_ms = data.time[0] * 1000;
+    var ns_to_ms = data.time[1] / 1000000;
+    $('.time').text(s_to_ms + ns_to_ms + 'ms');
+  }
 })
