37f3b6cddd9d9b5a469118943c7402a29f66540b
Michael Gillett <mgillett54@gmail.com>
2015-08-16 21:42:51 -0400

    Added timer to determine how long a query takes from start to finish Restructured callbacks to send more information back to the client Updated the web UI to display all relevant information

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,11 @@ $(function() {
   $('.btn').click(function() {
     var query = $('.query').val();
     $.post('/query', {query: query}, function(data) {
-      $('.result').show().text(data)
+      $('.response').text(data.res);
+      $('.result').show().text(JSON.stringify(data.raw, null, 2));
+      var s_to_ms = data.time[0] * 1000;
+      var ns_to_ms = data.time[1] / 1000000;
+      $('.time').text(s_to_ms + ns_to_ms + 'ms');
     })
   })
 
